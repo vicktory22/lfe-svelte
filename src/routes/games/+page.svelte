a688cbc06228864cb73ef411f97af6c4aebf9add
Tim Vick <t.vick@yahoo.com>
2023-09-30 08:53:15 -0700

    feat: adds games pages (#8)

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,1 +1,17 @@
-<h1 class="text-xl">Games</h1>
+<script lang="ts">
+	import Game from '$lib/Game.svelte';
+	import Nav from '$lib/Nav.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<Nav>
+	<h1 class="text-xl">Games</h1>
+
+	{#each data.games as game}
+		<div class="flex flex-col">
+			<Game {game} />
+		</div>
+	{/each}
+</Nav>
---
+++ src/routes/games/[id]/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Nav from '$lib/Nav.svelte';
+	import TeamWithSpread from '$lib/TeamWithSpread.svelte';
+
+	export let data: any;
+
+	const game = data.game;
+</script>
+
+<Nav>
+	<div class="flex flex-col justify-center p-2 h-screen">
+		<div>
+			<TeamWithSpread team={game.awayTeam} spread={-game.games.spread} />
+		</div>
+
+		<div class="divider">OR</div>
+
+		<div>
+			<TeamWithSpread team={game.homeTeam} spread={game.games.spread} />
+		</div>
+		<div class="mt-6">
+			<button class="btn btn-solid-warning btn-block drop-shadow-lg" on:click={() => goto('/games')}
+				>Cancel</button
+			>
+		</div>
+	</div>
+</Nav>
