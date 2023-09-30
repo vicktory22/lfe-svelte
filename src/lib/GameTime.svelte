3d3487c10c0ce5afa8bcc628c86ea0f8de425201
Tim Vick <t.vick@yahoo.com>
2023-09-30 08:39:56 -0700

    chore: adds initial components

---
+++ src/lib/GameTime.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { isBefore } from 'date-fns';
+	import { format } from 'date-fns-tz';
+
+	export let gameTime;
+
+	const time = format(gameTime, 'hh:mm a');
+	const timeZone = format(gameTime, 'z', { timeZone: 'America/Los_Angeles' });
+
+	const hasStarted = isBefore(new Date(), gameTime);
+</script>
+
+<div class="text-xs">
+	{#if hasStarted}
+		<span class="font-bold">{time}</span>
+		<span class="pl-1 self-start">{timeZone}</span>
+	{:else}
+		<span class="font-bold text-error">Game started</span>
+	{/if}
+</div>
