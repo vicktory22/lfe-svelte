38fbc8f94c75fd7490caed0d12bd5d6c158c93c0
Tim Vick <t.vick@yahoo.com>
2023-09-30 09:16:49 -0700

    chore: adds initial server hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,20 @@
+import { SECRET_DB_URL, SECRET_DB_AUTH_TOKEN } from "$env/static/private";
+import type { Handle } from '@sveltejs/kit';
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+import { getWeekId } from '$lib/server/weeks.service';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  // load database
+  event.locals.db = drizzle(createClient({ url: SECRET_DB_URL, authToken: SECRET_DB_AUTH_TOKEN }));
+
+  // load weekId
+  event.locals.weekId = getWeekId()!;
+
+  if (event.url.pathname.startsWith('/api/games')) {
+    console.log('cheking cookies');
+  }
+
+  const response = await resolve(event);
+  return response;
+}
