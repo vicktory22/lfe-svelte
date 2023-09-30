f21a7bbbcc4d5aacd867e2f647119bdaa2e06125
Tim Vick <t.vick@yahoo.com>
2023-09-30 09:31:03 -0700

    chore: adds base types (#11)
    * feat: adds some base types
    
    * chore: adds initial server hooks
    
    * chore: adds dummy favico
    
    * fix: adds types as optional

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { LibSQLDatabase } from "drizzle-orm/libsql";
+
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
+    interface Locals {
+      db: LibSQLDatabase;
+      weekId: number;
+    }
+    interface PageData {
+      games?: any[];
+      game?: any;
+    }
     interface Platform {
       env: {
         COUNTER: DurableObjectNamespace;
